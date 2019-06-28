3ab3f96a6be7ac3d753b9669d052ec6884db381b
phamtu123 <dendroidking02121@gmail.com>
2019-06-28 17:13:25 +0700

    modify:face

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,42 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "CRM Facebook Lead Ads",
-
-    'summary': """
-        Sync Facebook Leads with Odoo CRM""",
-
-    'description': """
-    """,
-
-    'author': "BADEP",
-    'website': "https://badep.ma",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Lead Automation',
-    'version': '11.0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['crm'],
-    'images': ['static/src/img/banner.png'],
-    'license': 'AGPL-3',
-
-    # always loaded
+    'name': "OMI Core",
+    'author': 'Odoo Community Association (OCA)',
+    'version': '11.0.1.0.0',
+    'maintainer': 'Lorem',
+    'website': "http://google.com",
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'sequence': 1000,
+    'summary': """Lorem ipsum dolor sit amet.""",
+    'depends': ['base', 'mail', 'crm', 'sale', 'note', 'im_livechat', 'crm_livechat', 'contacts'],
     'data': [
         'security/ir.model.access.csv',
-        'views/crm_view.xml',
-    ]
+        'data/omi_fb_permission.xml',
+        'views/assets.xml',
+        'views/omi_fb_permission_view.xml',
+        'views/omi_quick_reply_view.xml',
+        'views/mail_channel_view.xml',
+        'views/res_partner_view.xml',
+        'views/res_config_settings_view.xml',
+        'views/menuitem.xml',
+    ],
+    'demo': [],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    # 'js': ['static/src/js/first_module.js',],
+    # 'css': ['static/src/css/web_example.css',],
+    # 'images': ['static/description/icon.png',],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [
+            'facebook',
+            'pymessenger',
+        ]
+    }
+    # 'pre_init_hook': 'pre_init_hook',
+    # 'post_init_hook': 'post_init_hook',
+    # 'uninstall_hook': 'uninstall_hook',
 }
